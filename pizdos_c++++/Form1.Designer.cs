ae7cd54e7803dc571d9655586b2002bdce2d7b3f
Dmitry Dashkov <idimon66@gmail.com>
2019-10-30 19:51:03 +0200

    init

---
+++ pizdos_c++++/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace pizdos_c____
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.formsPlot1 = new ScottPlot.FormsPlot();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // formsPlot1
+            // 
+            this.formsPlot1.Location = new System.Drawing.Point(155, 12);
+            this.formsPlot1.Name = "formsPlot1";
+            this.formsPlot1.Size = new System.Drawing.Size(633, 426);
+            this.formsPlot1.TabIndex = 0;
+            this.formsPlot1.Load += new System.EventHandler(this.FormsPlot1_Load);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Green;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 48);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(94, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(112, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(37, 21);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "R";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(12, 75);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(137, 363);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(794, 329);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(45, 80);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(794, 415);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(45, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(851, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.formsPlot1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ScottPlot.FormsPlot formsPlot1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ pizdos_c++++/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace pizdos_c____
+{
+    public partial class Form1 : Form
+    {
+
+        struct rx_data
+        {
+            public int[] pin_status;
+            public int time_tick;
+        }
+
+        List<rx_data> list_data = new List<rx_data>();
+        Int64 X_Axis_Base = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SerialPortReset()
+        {
+            foreach (var port_name in SerialPort.GetPortNames())
+            {
+                if(!comboBox1.Items.Contains(port_name))
+                {
+                    comboBox1.Items.Add(port_name);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SerialPortReset();
+        }
+
+        private void FormsPlot1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (!serialPort1.IsOpen && comboBox1.SelectedIndex != -1)
+            {
+                serialPort1.PortName = ((string)comboBox1.SelectedItem);
+                serialPort1.Open();
+
+                char[] st = { 's', 't', 'a', 'r', 't' };
+                serialPort1.Write(st, 0, 5);
+                button1.Text = "STOP";
+                button1.BackColor = System.Drawing.Color.Red;
+                timer1.Enabled = true;
+                return;
+            }
+
+            if (serialPort1.IsOpen)
+            {
+                char[] st = { 's', 't', 'o', 'p' };
+                serialPort1.Write(st, 0, 4);
+                serialPort1.Close();
+                button1.Text = "START";
+                button1.BackColor = System.Drawing.Color.Green;
+                timer1.Enabled = false;
+                return;
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            SerialPortReset();
+        }
+
+        //разбираю полученные данные
+        private void ReadRxData(string data)
+        {
+            rx_data result;
+            int[] pin_status = new int[8];
+            for (int i = 0; i < 8; i++)
+            {
+                pin_status[i] = Convert.ToInt32(data[i]) - 48;
+            }
+            result.pin_status = pin_status;
+
+            string time = "";
+            for (int i = 9; i < 15; i++)
+            {
+                time += data[i];
+            }
+            result.time_tick = Int32.Parse(time);
+
+            list_data.Add(result);
+        }
+
+        //русую 
+        private void DrawRxData()
+        {
+            //formsPlot1.plt.PlotScatter(list_data.ToArray().time_tick, list_data.ToArray()[].pin_status[0]);
+            // Выделяю восемь листов пушо могу себе позволить
+            List<double> pin0 = new List<double>();
+            List<double> pin1 = new List<double>();
+            List<double> pin2 = new List<double>();
+            List<double> pin3 = new List<double>();
+            List<double> pin4 = new List<double>();
+            List<double> pin5 = new List<double>();
+            List<double> pin6 = new List<double>();
+            List<double> pin7 = new List<double>();
+            List<double> time_tick = new List<double>();
+            foreach (var data in list_data)
+            {
+                pin0.Add(data.pin_status[0]);
+                pin1.Add(data.pin_status[1]);
+                pin2.Add(data.pin_status[2]);
+                pin3.Add(data.pin_status[3]);
+                pin4.Add(data.pin_status[4]);
+                pin5.Add(data.pin_status[5]);
+                pin6.Add(data.pin_status[6]);
+                pin7.Add(data.pin_status[7]);
+
+                time_tick.Add(data.time_tick);
+            }
+            //formsPlot1.plt.PlotScatter(time_tick.ToArray(), pin0.ToArray());
+            //formsPlot1.Render();
+
+            formsPlot1.plt.Clear();
+            formsPlot1.plt.PlotSignal(pin0.ToArray(), markerSize: 0);
+            formsPlot1.plt.PlotSignal(pin1.ToArray(), markerSize: 0);
+            formsPlot1.plt.PlotSignal(pin2.ToArray(), markerSize: 0);
+            formsPlot1.plt.PlotSignal(pin3.ToArray(), markerSize: 0);
+            formsPlot1.plt.PlotSignal(pin4.ToArray(), markerSize: 0);
+            formsPlot1.plt.PlotSignal(pin5.ToArray(), markerSize: 0);
+            formsPlot1.plt.PlotSignal(pin6.ToArray(), markerSize: 0);
+            formsPlot1.plt.PlotSignal(pin7.ToArray(), markerSize: 0);
+
+            if (X_Axis_Base < 1000)
+            {
+                formsPlot1.plt.Axis(y1: -0.2, y2: 1.2, x1: 0, x2: 1000);
+            }
+            else
+            {
+                formsPlot1.plt.Axis(y1: -0.2, y2: 1.2, x1: X_Axis_Base - 1000, x2: X_Axis_Base);
+            }
+
+            X_Axis_Base++;
+            //formsPlot1.plt.AxisAutoX();
+            textBox2.Invoke((MethodInvoker)delegate
+            {
+                textBox2.Text = pin0.Count.ToString();
+            });
+            formsPlot1.Render();
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            char[] byte_rx = new char[15];
+            serialPort1.Read(byte_rx, 0, 15);
+            serialPort1.DiscardInBuffer();
+
+            string serial_rx = "";
+
+            for (int i = 0; i < 15; i++)
+            {
+                serial_rx += byte_rx[i];
+            }
+
+            textBox1.Invoke((MethodInvoker)delegate
+            {
+                textBox1.Text += serial_rx;
+                textBox1.Text += '\n';
+                textBox1.SelectionStart = textBox1.Text.Length;
+                textBox1.ScrollToCaret();
+            });
+
+            ReadRxData(serial_rx);
+            DrawRxData();
+        }
+    }
+}
